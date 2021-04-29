22a0d13f5983baec76d52fd2287932dab67fe9b7
Hakim Asrori <saneglos005@gmail.com>
2021-04-30 05:26:12 +0700

    Membenahi layout.foot.php

--- layout/foot.php
+++ layout/foot.php
@@ -1,10 +1,10 @@
 		</div>
 	</div>
 
-	<script src="<?= '/vendor/jquery/jquery.min.js' ?>"></script>
-	<script src="<?= '/vendor/bootstrap/js/bootstrap.bundle.min.js' ?>"></script>
-	<script src="<?= '/js/bootstrap.min.js' ?>"></script>
-	<script src="<?= '/vendor/lightbox2/js/lightbox-plus-jquery.min.js' ?>"></script>
+	<script src="<?= '/assets/vendor/jquery/jquery.min.js' ?>"></script>
+	<script src="<?= '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js' ?>"></script>
+	<script src="<?= '/assets/js/bootstrap.min.js' ?>"></script>
+	<script src="<?= '/assets/vendor/lightbox2/js/lightbox-plus-jquery.min.js' ?>"></script>
 </body>
 
 </html>
